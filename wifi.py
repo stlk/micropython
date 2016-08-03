0ef79b73c0d32007b5f88cff0922c596c942b996
Josef Rousek <josef.rousek@gmail.com>
2016-08-03 22:45:39 +0200

    webREPL and wifi

---
+++ wifi.py
@@ -0,0 +1,10 @@
+def do_connect():
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect('ssid', 'pass')
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
