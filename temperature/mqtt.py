6a9f9b1db524a2392fa08a5f77f1640439ac367a
Josef Rousek <josef.rousek@gmail.com>
2016-08-06 16:45:45 +0200

    Handle connection failures

--- temperature/mqtt.py
+++ temperature/mqtt.py
@@ -2,9 +2,21 @@ from simple import MQTTClient
 from settings import MQTT_HOSTNAME
 
 c = MQTTClient('umqtt_client', MQTT_HOSTNAME)
-c.connect()
-c.publish(b'status', b'hi')
 
+try:
+    c.connect()
+    c.publish(b'status', b'hi')
+except:
+    print('MQTT hello failed')
+
+
+def publish(temp):
+    c.publish(b'temp', b'%s' % (temp,))
 
 def send(temp):
-    c.publish(b'temp', b'%s' % (temp,))
+    try:
+        publish(temp)
+    except:
+        print('MQTT retry')
+        c.connect()
+        publish(temp)
