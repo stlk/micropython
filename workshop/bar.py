ab352d5be70c2d58243920e91480b90f2d0ef423
Josef Rousek <josef.rousek@gmail.com>
2017-01-22 10:46:29 +0100

    Use white for temperature between -1 and +1

--- workshop/bar.py
+++ workshop/bar.py
@@ -11,14 +11,18 @@ np = NeoPixel(pin, NEOPIXEL_COUNT)
 RED = (16, 16, 0)
 BLUE = (0, 0, 16)
 OFF = (0, 0, 0)
+WHITE = (16, 16, 16)
 
 def set_panel(temp):
     color = RED if temp > 0 else BLUE
     for i in range(NEOPIXEL_COUNT):
-        np[i] = color if abs(temp) >= i + 1 else OFF
+        if temp > -1 and temp < 1:
+            np[i] = WHITE
+        else:
+            np[i] = color if abs(temp) >= i + 1 else OFF
     np.write()
 
 while True:
     temp = temperature.read_temp()
     set_panel(temp)
-    time.sleep(1)
+    time.sleep(5)
